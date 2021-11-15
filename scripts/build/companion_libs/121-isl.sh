6d2ee0bfe5e93dd5a3916c98904b7c7c0564f827
jomasnash <51049887+jomasnash@users.noreply.github.com>
2021-11-15 14:06:33 +0100

    Update 121-isl.sh
    replace broken link to source

--- scripts/build/companion_libs/121-isl.sh
+++ scripts/build/companion_libs/121-isl.sh
@@ -14,7 +14,7 @@ if [ "${CT_ISL}" = "y" ]; then
 # Download ISL
 do_isl_get() {
     CT_GetFile "isl-${CT_ISL_VERSION}" \
-        http://isl.gforge.inria.fr
+        https://gcc.gnu.org/pub/gcc/infrastructure
 }
 
 # Extract ISL
